87dd5339f21b643cf979c776bee9a9f8035398cf
Toshiaki Maki <tmaki@vmware.com>
2023-01-12 20:03:50 +0900

    first commit

---
+++ src/main/java/am/ik/accesslogger/AccessLogger.java
@@ -0,0 +1,70 @@
+package am.ik.accesslogger;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.actuate.web.exchanges.HttpExchange;
+import org.springframework.boot.actuate.web.exchanges.HttpExchange.Principal;
+import org.springframework.boot.actuate.web.exchanges.HttpExchange.Request;
+import org.springframework.boot.actuate.web.exchanges.HttpExchange.Response;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.util.CollectionUtils;
+
+public class AccessLogger implements HttpExchangeRepository {
+	private final Predicate<HttpExchange> filter;
+
+	private final Logger log = LoggerFactory.getLogger("accesslog");
+
+	public AccessLogger(Predicate<HttpExchange> filter) {
+		this.filter = filter;
+	}
+
+	public AccessLogger() {
+		this(x -> true);
+	}
+
+	@Override
+	public List<HttpExchange> findAll() {
+		return List.of();
+	}
+
+	@Override
+	public void add(HttpExchange httpExchange) {
+		final Request request = httpExchange.getRequest();
+		final URI uri = request.getUri();
+		if (!filter.test(httpExchange)) {
+			return;
+		}
+		final Response response = httpExchange.getResponse();
+		final Principal principal = httpExchange.getPrincipal();
+		final Duration timeTaken = httpExchange.getTimeTaken();
+		final Map<String, List<String>> headers = request.getHeaders();
+		final StringBuilder log = new StringBuilder();
+		log.append("remote=").append(request.getRemoteAddress()).append(" ");
+		if (principal != null) {
+			log.append("user=").append(principal.getName()).append(" ");
+		}
+		log.append("timestamp=").append(httpExchange.getTimestamp()).append(" ");
+		log.append("method=").append(request.getMethod()).append(" ");
+		log.append("url=").append(uri).append(" ");
+		log.append("status=").append(response.getStatus()).append(" ");
+		final List<String> referer = headers.get("referer");
+		if (!CollectionUtils.isEmpty(referer)) {
+			log.append("referer=\"").append(referer.get(0)).append("\" ");
+		}
+		final List<String> userAgent = headers.get("user-agent");
+		if (!CollectionUtils.isEmpty(userAgent)) {
+			log.append("ua=\"").append(userAgent.get(0)).append("\" ");
+		}
+		if (timeTaken != null) {
+			log.append("response_time=").append(timeTaken.toMillis()).append(" ");
+		}
+		this.log.info(log.toString().trim());
+	}
+}
