8cc09a41dc66b079a3a2d21f45409b829e6b5c84
Toshiaki Maki <tmaki@vmware.com>
2023-01-13 09:31:44 +0900

    Update scripts

--- set-next-patch-version.sh
+++ set-next-patch-version.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-CURRENT_VERSION=$(grep '<version>' pom.xml | head -n 1 | sed -e 's|<version>||g' -e 's|</version>||g' -e 's| ||g' |  tr -d '\t' | sed 's/-SNAPSHOT$//')
+CURRENT_VERSION=$(./get-release-version.sh)
 MAJOR_VERSION=$(echo ${CURRENT_VERSION} | awk -F '.' '{print $1}' )
 MINOR_VERSION=$(echo ${CURRENT_VERSION} | awk -F '.' '{print $2}' )
 PATCH_VERSION=$(echo ${CURRENT_VERSION} | awk -F '.' '{print $3}' )
--- set-release-version.sh
+++ set-release-version.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-CURRENT_VERSION=$(grep '<version>' pom.xml | head -n 1 | sed -e 's|<version>||g' -e 's|</version>||g' -e 's| ||g' |  tr -d '\t' | sed 's/-SNAPSHOT$//')
+CURRENT_VERSION=$(./get-release-version.sh)
 
 ./mvnw versions:set -DnewVersion=${CURRENT_VERSION} -DgenerateBackupPoms=false
 git add pom.xml
